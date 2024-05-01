e90039388b623fe88812666cbfa2ce61b59a6928
GinaGanaa <otgoo.gantsetseg@gmail.com>
2024-04-30 23:58:38 -0400

    dashboard

--- src/components/SideBarButton.jsx
+++ src/components/SideBarButton.jsx
@@ -5,7 +5,7 @@ export const SideBarButton = ({ selectedMenu, item, setSelectedMenu }) => {
     <li
       style={{ background: selectedMenu == item.name ? "#4880FF" : "white" }}
       onClick={() => setSelectedMenu(item.name)}
-      className="pointer flex bg-blue-600 h-[50px] w-[180px] rounded-md items-center gap-4 pl-5  "
+      className="pointer flex bg-blue-600 h-[50px] w-[180px] rounded-md items-center gap-4 pl-5"
     >
       <img src={item.img} alt="" />
       <div>
