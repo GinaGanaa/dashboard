c728a89326c320719e1c80196329c0b3acf2ff51
GinaGanaa <otgoo.gantsetseg@gmail.com>
2024-11-18 23:22:32 -0500

    elements improvement

--- src/pages/element.jsx
+++ src/pages/element.jsx
@@ -22,10 +22,7 @@ export default function Page() {
       </div>
       <div className="h-[318px] bg-white mt-[16px] rounded-[14px]">
         <p className="p-[24px] text-[24px] font-medium ">Donut Chart</p>
-        <div className="flex justify-between pl-[72px]">
-          <img src="./elements/greenDonutChart.svg" alt="" />
-          <img src="" alt="" />
-        </div>
+        <div className="flex justify-between pl-[72px]"></div>
       </div>
     </div>
   );
